8c9e33152d074307af8d44822a0520411111d65e
JhonS27 <sumiljhon78@gmail.com>
2024-10-03 10:12:01 +0800

    Add files via upload

---
+++ Sumil04BasicInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string nickname;
+	string CYL;
+	string school;
+	
+	cout << "Nickname | ";
+	cin >> nickname;
+	cout << "Course-Year-Level | ";
+	cin >> CYL;
+	cout << "School |";
+	cin >> school;
+	
+	
+	return 0;
+}
