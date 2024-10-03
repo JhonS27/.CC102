ef2ed543fc348124e108f1c18c04eca089c07ed3
JhonS27 <sumiljhon78@gmail.com>
2024-10-03 09:56:12 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "School\t\t\t| Our Lady of Lourdes College Foundation" << endl;
+	cout << "Department\t\t\t| Bachelor of Science Computer Studies" << endl;
+	cout << "Course & Year Level\t\t| BSIT 1st Year" << endl;
+	cout << "Your Permanent Address\t\t| Paracale Camarines Norte" << endl;	
+	
+	return 0;
+}
